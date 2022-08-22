d24257ba02501d2aec6a6cebdc7dc62e42e4d4f5
SungDongIn <ddongin10@naver.com>
2022-08-22 16:50:50 +0900

    controller, service

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,25 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken, TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
-import { User } from './entity/user.enetity'
 import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
   
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
-      providers: [
-        UserService,
-        {
-          // getRepositoryToken => This function generates an injection token for an Entity or Repository
-          provide: getRepositoryToken(User),
-          // fn => Creates a mock function. Optionally takes a mock implementation.
-          useValue: jest.fn()
-        }
-      ]
+      providers: [UserService]
     }).compile();
 
     controller = module.get<UserController>(UserController);
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,25 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { User } from './entity/user.enetity';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
   let service: UserService;
 
-  // When it comes to unit testing an application, we usually want to avoid making a database connection,
-  // keeping our test suites independent and their execution process as fast as possible. 
-  const mockRepository = {
-    getOnea: jest.fn(() => console.log("heelo"))
-  }
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        UserService,
-        {
-          provide: getRepositoryToken(User),
-          useValue: mockRepository
-        }],
+        UserService]
     }).compile();
 
     service = module.get<UserService>(UserService);
